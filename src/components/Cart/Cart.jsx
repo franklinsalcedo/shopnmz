f0c83c6d6151f996ebac0cc58e2e2cf80a6b95b6
Franklin <fsalcedo@gmail.com>
2021-01-14 20:32:30 -0300

    Mejoras y ajustes en cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,14 +1,25 @@
-import { useState, useContext } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { products } from '../../products';
 import { Store } from '../../store';
 
 
 function Cart() {
-    const [items, setItems] = useState([]);
-    const [data, SetData] = useContext(Store);
+    const [data, setData] = useContext(Store);
+    const [listProducts] = useState(products);
+    const [selects, setSelects] = useState([]);
+    
+    const getStoreData = () => {
+        const dataStore = data.items;
+        dataStore.forEach((item,index) => {
+            let sel = listProducts.filter(product => product.id === item.itemId);
+            selects.push(sel);
+        });
+        console.log(selects);
+    }
 
-    
-    
+    useEffect(() => {
+        getStoreData();
+    })
 
     return (
         <section className="container" id="cart">
@@ -18,18 +29,27 @@ function Cart() {
                 </div>
             </div>
             {
-                data.items.map(item => 
-                     
-                    <p>{item.itemId}</p>    
-                )
-            }
-            <div className="row">
-                <div className="col-12">
-                    Nombre: <br/>
-                    Precio: <br/>
-                    Cantidad: 
+                selects.length ?
+                selects.map((item, index) => (
+                    <div className="row">
+                        <div className="col-12">
+                            Nombre: { item[0].title }
+                        </div>
+                        <div className="col-12">
+                            Precio: ${ item[0].price }
+                        </div>
+                        <div className="col-12">
+                            Cantidad:
+                        </div>
+                    </div>
+                ))
+                :
+                <div className="row">
+                    <div className="col-12 text-center">
+                        <p>Carrito vacío...</p>
+                    </div>
                 </div>
-            </div>
+            }
         </section>
     );
 }
